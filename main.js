cc16c5c391d1e6a77d877d55217cd3e14fe4ecf0
felix <fillefish@hotmail.com>
2021-07-17 03:46:35 +0200

    tons of stuff

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './scss/main.scss';
 
 import {burgerMenu} from "./js/components/burgermenu.js"
 import {buttonEvents} from "./js/components/buttons.js"
+import {gsapAnimations} from "./js/components/scrollanimations.js"
+
 
 window.addEventListener("DOMContentLoaded", init);
 
@@ -10,6 +12,7 @@ function init() {
   console.log("initialization complete!");
   burgerMenu();
   buttonEvents();
+  gsapAnimations();
   
   
 }
